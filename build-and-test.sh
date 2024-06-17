93cc3e5e3cf1533fd3c8bc4d631e2449de6801ff
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2024-06-17 22:14:31 +0200

    Add RUSTFLAGS env to build script

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,1 +1,2 @@
-cargo build && hsh --plugin hello hello.hsl
+# https://doc.rust-lang.org/rustc/codegen-options/index.html#relro-level
+RUSTFLAGS "-C relro-level=partial" cargo build && hsh --plugin hello hello.hsl
