aa9188ae90a79a06469697755624dac13427c921
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2024-06-17 21:44:12 +0000

    Tweak 93cc3e5e3cf1533fd3c8bc4d631e2449de6801ff

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,2 +1,2 @@
 # https://doc.rust-lang.org/rustc/codegen-options/index.html#relro-level
-RUSTFLAGS "-C relro-level=partial" cargo build && hsh --plugin hello hello.hsl
+RUSTFLAGS="-C relro-level=partial" cargo build && hsh --plugin hello hello.hsl
